308b34dce9fe2a865c563cd4f5228ce1516a0345
jlabrego <jessicalourdabrego2004@gmail.com>
2026-08-01 21:23:46 -0600

    chore: configurar estructura inicial y base de datos con Docker

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import dotenv from 'dotenv/config'
+import { pool } from './src/db/db.js'
+
+
+const app = express()
+app.use(express.json())
+
+
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send('API Facturación funcionando')
+})
+try {
+    const connection = await pool.getConnection()
+    console.log('✅ Base de datos conectada')
+    connection.release()
+} catch (error) {
+    console.error('❌ Error al conectar la base de datos')
+    console.error(error.message)
+}
+app.listen(PORT,() => {
+    console.log(`Servidor en marcha en: htpp://localhost: ${PORT}`)
+})
+
