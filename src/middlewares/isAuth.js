cc23a22b96507ad2b8e2b24c92c96586bd6f49ef
jlabrego <jessicalourdabrego2004@gmail.com>
2026-08-02 21:20:19 -0600

    Implementación del middleware de autenticación

--- src/middlewares/isAuth.js
+++ src/middlewares/isAuth.js
@@ -1,24 +1,27 @@
 import jwt from 'jsonwebtoken'
-import { jsonResponse } from '../../src/helpers/json_response.js'
+import { jsonResponse } from '../helpers/json_response.js'
 
 export const isAuth = async (req, res, next) => {
 
     // capturar la req y obtener los encabezados
+    if(!req.headers.authorization){
+        return res.status(401).json(jsonResponse({
+            status: 401,
+            message: 'Debe iniciar sesión',
+            data: null
+        }))
+    }
     const token = req.headers.authorization.split(' ')[1]
+
 
     //validar la data
     try {
-        const { id, permissions } = jwt.verify(token, process.env.JWT_SECRET)
+        const payload = jwt.verify( token, process.env.JWT_SECRET)
 
-        const user = {
-            id, permissions
-        }
-        req.user = user
-        // req.headers.user.permission = permission
-
-        //puede continaur
+        req.user = payload
         next()
-    } catch (e) {
+    } 
+    catch (e) {
         console.log(e)
         return res.status(401).json(jsonResponse({
             status: 401,
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import {login} from '../controllers/auth.controller.js'
-
+import {isAuth} from '../middlewares/isAuth.js'
 const authRoutes = Router()
 
 console.log('AuthRoutes cargadas')
